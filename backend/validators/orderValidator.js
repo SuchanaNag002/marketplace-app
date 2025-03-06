9131191f7b09b9e1ca85b438ba41c7c79f4c6e53
SuchanaNag002 <suchananag2002@gmail.com>
2025-03-06 13:33:43 +0530

    order validator

--- backend/validators/orderValidator.js
+++ backend/validators/orderValidator.js
@@ -4,6 +4,10 @@ const orderValidator = Joi.object({
   productId: Joi.string().required().messages({
     'string.empty': 'Product ID is required'
   }),
+  quantity: Joi.string().optional().default('0'),
+  shippingAddress: Joi.string().required().messages({
+    'string.empty': 'Shipping address is required'
+  }),
   orderStatus: Joi.string().optional().default('Pending'),
   orderDate: Joi.date().optional().default(() => new Date(), 'current date')
 });
