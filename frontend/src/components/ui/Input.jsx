362f11bd509166c61654f727d28c1d74b9347809
SuchanaNag002 <suchananag2002@gmail.com>
2025-03-06 15:34:55 +0530

    input field issue

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
-const Input = ({ label, type = "text", placeholder, value, onChange }) => {
+const Input = ({ label, type = "text", placeholder, name, value, onChange }) => {
   return (
     <div className="w-full mb-4">
       <label className="block text-sm font-medium text-gray-700 mb-1">{label}</label>
       <input
         type={type}
+        name={name}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="w-full px-2 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+        className="w-full px-2 py-2 border border-gray-300 rounded-lg text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500"
       />
     </div>
   );
